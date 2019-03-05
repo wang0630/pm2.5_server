07b028cb340c6db5e267f045d08bd42c52a359c0
wang0630 <j2081499@gmail.com>
2019-03-05 20:44:19 +0800

    Add pylintrc and readme

--- server/server.py
+++ server/server.py
@@ -4,36 +4,40 @@ import json
 from db import connectToDB
 from models.data_model import PMData
 # Request handler
-def handler(sock,addr):
+def handler(sock):
     msg = 'YEE from server. YEEEEEEEEEE'
 
     sock.send(msg.encode('utf-8'))
 
     while True:
-        msg=sock.recv(1024)
+        msg = sock.recv(1024)
         if not msg:
             print("ERROR : No data")
         else:
             # json format must be double quoted instead of being single quoted
             data = json.loads(msg.decode('utf-8').replace("\'", "\""))
             # unpacking the tuple
-            PMData(*tuple(data.values())).save();
-   
+            PMData(*tuple(data.values())).save()
+
     msg = 'Closing connection...'
     sock.send(msg.encode('utf-8'))
     sock.close()
-if __name__ == '__main__':
+
+def main():
     # Turn on server
-    sock = socket.socket()
-    sock.bind(('0.0.0.0', 8080))# port
-    sock.listen(5)
+    mainSock = socket.socket()
+    mainSock.bind(('0.0.0.0', 8080))# port
+    mainSock.listen(5)
     # Connect to mongodb
     connectToDB()
     # testing for insertion
-    
+
     print('Waiting for connection...')
     while True:
-        (socket,addr) = sock.accept()
+        (soAccept, addr) = mainSock.accept()
         # Create a new thread to handle requests
-        thread = threading.Thread(target=handler,args=(socket,addr))
+        thread = threading.Thread(target=handler, args=(soAccept, addr))
         thread.start()
+
+if __name__ == '__main__':
+    main()
