405b69a4044b17b4feeee199b131300373b65388
YOYOPIG <yoyopig@localhost.localdomain>
2019-03-22 14:06:02 +0800

    HTTP server with POST handler

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 # PM2.5 Prediction Server
-This is a program that receives data sent by the sensors and saves into the database (mongoDB).
+This repository includes all the programs to be run on Synology NAS, along with docker setting files. This includes : 
+* server - This is the server that receives data sent by the sensors and saves into the database (mongoDB).
+* http_server - This is the server that handles http request from the mobile app, retrieves the requested data from mongoDB and sends back to the users
 
+## Requirements
+Docker
 
-# How to turn on both server and mongodb
+## How to turn on servers and mongodb
 1. ssh to the machine and sudo -i
 2. navigate to volumes1/docker/server
 3. docker-compose up --build --force-recreate -d
 
-# run pylint
+## run pylint
 navigate to project directory and input pylint **/*.py
 
-# check logs
+## check logs
 docker-compose logs
--- http_server/flask_server.py
+++ http_server/flask_server.py
@@ -1,8 +1,8 @@
+import json
 from flask import Flask
 from flask import jsonify
 from flask import request
 from flask_pymongo import PyMongo
-import json
 
 # Create Flask app
 app = Flask(__name__)
@@ -11,38 +11,41 @@ app = Flask(__name__)
 app.config['MONGO_DBNAME'] = 'pmBase'
 app.config['MONGO_URI'] = 'mongodb://mongo:27017/pmBase'
 
-mongo = PyMongo(app)
-if mongo:
-	print('Connected to mongo')
+MONGO = PyMongo(app)
+if MONGO:
+    print('Connected to mongo')
 else:
-	print('Failed connecting to mongo')
+    print('Failed connecting to mongo')
 
 # Handle POST requests
 @app.route('/', methods=['POST'])
 def handle_post_rq():
-	data = request.get_json()
-	position = data.get('position', 'no pos')
-	if position == 'no pos':
-		app.logger.info('Bad request')
-		return 'ERROR : Bad request, no position', 400
-	else:
-		return get_recent_data(position)
+    data = request.get_json()
+    position = data.get('position', 'no pos')
+    if position == 'no pos':
+        app.logger.info('Bad request')
+        return 'ERROR : Bad request, no position', 400
+    else:
+        return get_recent_data(position)
 
 # Get the most recent data in the given position
 # pos : The ID of the sensor to be queried
 # Return value : The jsonified data that is ready to be sent, 404 if any error occured
 def get_recent_data(pos):
-	try:
-		target_data = mongo.db.pm_data
-		return_data = list(target_data.find({'position' : pos}).sort([('_id', -1)]).limit(1)) # sort data backwards, gets the newest data
-		result = {'position' : return_data[0].get('position'), 'date' : return_data[0].get('date'), 'temperature' : return_data[0].get('temp'),
-			'humidity' : return_data[0].get('humidity'), 'pm10' : return_data[0].get('pm10'), 'pm25' : return_data[0].get('pm25'),
-			'pm100' : return_data[0].get('pm100')}
-		return jsonify(result = result) 
-	except:
-		app.logger.info('ERROR getting data ')
-		result = "Oops! Can't find data from the sensor with that id."
-		return result, 404
+    try:
+        target_data = MONGO.db.pm_data
+        # sort data backwards, gets the newest data
+        return_data = list(target_data.find({'position' : pos}).sort([('_id', -1)]).limit(1))
+
+        result = {'position' : return_data[0].get('position'), 'date' : return_data[0].get('date'),
+                  'temperature' : return_data[0].get('temp'), 'humidity' : return_data[0].get('humidity'),
+                  'pm10' : return_data[0].get('pm10'), 'pm25' : return_data[0].get('pm25'),
+                  'pm100' : return_data[0].get('pm100')}
+        return jsonify(result=result)
+    except:
+        app.logger.info('ERROR getting data ')
+        result = "Oops! Can't find data from the sensor with that id."
+        return result, 404
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0',debug=True)
+    app.run(host='0.0.0.0', debug=True)
